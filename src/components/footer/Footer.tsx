b79dc9cb3b11fd652991f010547b1131a8dd96a6
Ada Pauline Villacarlos <adavillacarlos@outlook.com>
2024-09-21 16:01:10 +0800

    added links

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,12 +5,12 @@ export default function Footer() {
     <footer>
       <div className="w-full mx-auto max-w-screen-xl flex flex-col items-center md:flex-row md:items-center mt-16 md:mt-24 md:py-10 py-5 px-6 md:px-20 md:justify-between">
         <span className="text-sm dark:text-white text-center sm:text-left">
-          Developed by Ada Villacarlos
+          Developed by Ada Villacarlos 💖
         </span>
         <ul className="flex flex-row items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 md:flex-row sm:mt-0">
           <li>
             <a
-              href="#"
+              href="mailto:adapauline.villacarlos@gmail.com"
               className="hover:underline me-4 md:me-6 mb-2 md:mb-0 text-base"
             >
               Mail
@@ -18,7 +18,7 @@ export default function Footer() {
           </li>
           <li>
             <a
-              href="#"
+              href="https://www.linkedin.com/in/adavillacarlos"
               className="hover:underline me-4 md:me-6 mb-2 md:mb-0 text-base"
             >
               LinkedIn
@@ -26,14 +26,17 @@ export default function Footer() {
           </li>
           <li>
             <a
-              href="#"
+              href="https://github.com/adavillacarlos"
               className="hover:underline me-4 md:me-6 mb-2 md:mb-0 text-base"
             >
               GitHub
             </a>
           </li>
           <li>
-            <a href="#" className="hover:underline text-base">
+            <a
+              href="https://www.behance.net/adavillacarlos"
+              className="hover:underline text-base"
+            >
               Behance
             </a>
           </li>
