857a083bf75dd01085d16ed400f5a69e316d9024
Ada Pauline Villacarlos <adavillacarlos@outlook.com>
2024-09-21 15:14:54 +0800

    removed unnused links

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,25 @@
-import { useEffect, useState } from "react";
 import About from "./components/about/About";
 import Experience from "./components/experience/Experience";
-import Navigation from "./components/navigation/Navigation";
-import Resume from "./components/resume/Resume";
 import Skills from "./components/skills/Skills";
 import Title from "./components/titles/Title";
+import Project from "./components/projects/Project";
+import Design from "./components/design/Design";
+import Footer from "./components/footer/Footer";
+import ScrollToTop from "./components/others/ScrollToTop";
 
 function App() {
   return (
     <div className="dark">
-      <Navigation />
       <div className=" dark:bg-gray-900">
         <Title />
         <About />
         <Skills />
         <Experience />
-        <Resume />
+        <Project />
+        <Design />
+        <Footer />
       </div>
+      <ScrollToTop/>
     </div>
   );
 }
