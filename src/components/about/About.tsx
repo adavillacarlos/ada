ac8aa727a595d2e8c6361b0a5dbcc397cf70d510
Ada Pauline Villacarlos <adavillacarlos@outlook.com>
2024-09-21 15:15:01 +0800

    updated about

--- src/components/about/About.tsx
+++ src/components/about/About.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function About() {
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-6 md:px-20 mt-12 md:mt-20 md:py-10  max-w-screen-lg mx-auto">
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 px-6 md:px-20 mt-12 md:mt-12 md:py-10  max-w-screen-lg mx-auto">
       <div className="flex justify-start md:justify-end md:col-span-1">
         <span className="font-bold text-left md:text-right pb-4 md:mr-20 text-blue-600 ">ABOUT</span>
       </div>
