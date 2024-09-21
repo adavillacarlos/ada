29ae19445d0edcced040a95f0af471bb76897e0c
Ada Pauline Villacarlos <adavillacarlos@outlook.com>
2024-09-21 15:15:11 +0800

    added footer and scrolltotop

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer>
+      <div className="w-full mx-auto max-w-screen-xl flex flex-col items-center md:flex-row md:items-center mt-16 md:mt-24 md:py-10 py-5 px-6 md:px-20 md:justify-between">
+        <span className="text-sm dark:text-white text-center sm:text-left">
+          Developed by Ada Villacarlos
+        </span>
+        <ul className="flex flex-row items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 md:flex-row sm:mt-0">
+          <li>
+            <a
+              href="#"
+              className="hover:underline me-4 md:me-6 mb-2 md:mb-0 text-base"
+            >
+              Mail
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="hover:underline me-4 md:me-6 mb-2 md:mb-0 text-base"
+            >
+              LinkedIn
+            </a>
+          </li>
+          <li>
+            <a
+              href="#"
+              className="hover:underline me-4 md:me-6 mb-2 md:mb-0 text-base"
+            >
+              GitHub
+            </a>
+          </li>
+          <li>
+            <a href="#" className="hover:underline text-base">
+              Behance
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+}
---
+++ src/components/others/ScrollToTop.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useState, useEffect } from "react";
+
+export default function ScrollToTop() {
+  const [isVisible, setIsVisible] = useState(false);
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setIsVisible(true);
+    } else {
+      setIsVisible(false);
+    }
+  };
+  // Scroll to the top
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", toggleVisibility);
+    return () => window.removeEventListener("scroll", toggleVisibility);
+  }, []);
+  return (
+    <div className="fixed bottom-8 right-8">
+      {isVisible && (
+        <button
+        onClick={scrollToTop}
+        className="p-3 transition duration-300 group"
+      >
+        <span className="text-3xl inline-block transition-transform duration-300 ease-out group-hover:animate-move-up">
+          ☝️
+        </span>
+      </button>
+      )}
+    </div>
+  );
+}
